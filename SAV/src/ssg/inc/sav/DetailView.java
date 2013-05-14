daaf2ee98b7cb5b27278481a2efba7c536ac124f
rua0831 <rua0831@gmail.com>
2013-05-14 10:36:28 +0900

    create new files

---
+++ SAV/src/ssg/inc/sav/DetailView.java
@@ -0,0 +1,14 @@
+package ssg.inc.sav;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class DetailView extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_view);
+    }
+
+}
---
+++ SAV/src/ssg/inc/sav/ListView.java
@@ -0,0 +1,13 @@
+package ssg.inc.sav;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class ListView extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_view);
+    }
+}
