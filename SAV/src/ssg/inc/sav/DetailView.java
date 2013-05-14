97b324d322bd40fc5d35f709d193ccdfc7e37c29
yaljel <yaljel@hanmail.net>
2013-05-14 14:18:09 +0900

    merge map

--- SAV/src/ssg/inc/sav/DetailView.java
+++ SAV/src/ssg/inc/sav/DetailView.java
@@ -1,14 +1,27 @@
 package ssg.inc.sav;
 
-import android.app.Activity;
 import android.os.Bundle;
 
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
 
-public class DetailView extends Activity {
+
+public class DetailView extends MapActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_view);
+        
+        MapView mapView = (MapView) findViewById(R.id.mapview);
+        mapView.setBuiltInZoomControls(true);
     }
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+    
+
 
 }
